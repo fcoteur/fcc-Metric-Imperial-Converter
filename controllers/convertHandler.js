fa9942c5ae4ec43f15ea99e7870013a2bab71221
Glitch (thoughtful-sundial) <none>
2018-08-25 14:00:59 +0000

    🌯🏬 First commit (using Glitch)

---
+++ controllers/convertHandler.js
@@ -0,0 +1,116 @@
+/*
+*
+*
+*       Complete the handler logic below
+*       
+*       
+*/
+
+function ConvertHandler() {
+  
+  this.getNum = function(input) {
+    var indexFristLetter = input.search(/[A-Za-z]+/g);
+    var result;
+    if (indexFristLetter=== 0) {result = 1} else {result = eval(input.slice(0,indexFristLetter));}
+    return result;
+  };
+  
+  this.getUnit = function(input) {
+    var result;
+    var indexFristLetter = input.search(/[A-Za-z]+/g);
+    var result = input.slice(indexFristLetter, input.length +1);  
+    return result;
+  };
+  
+  this.getReturnUnit = function(initUnit) {
+    var result;
+    switch (initUnit) {
+      case 'gal':
+        result = 'L';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'km':
+        result = 'mi';
+        break;
+      case 'kg':
+        result = 'mi';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      default:
+    };
+    return result;
+  };
+
+  this.spellOutUnit = function(unit) {
+    var result;
+    switch (unit) {
+      case 'gal':
+        result = 'gallons';
+        break;
+      case 'lbs':
+        result='pound';
+        break;
+      case 'mi':
+        result='miles';
+        break;
+      case 'L':
+        result='liters';
+        break;
+      case 'kg':
+        result='kilograms';
+        break;
+      case 'km':
+        result = 'kilometers';
+        break;
+      default:
+    };
+    return result;
+  };
+  
+  this.convert = function(initNum, initUnit) {
+    const galToL = 3.78541;
+    const lbsToKg = 0.453592;
+    const miToKm = 1.60934;
+    var result;    
+    switch (initUnit) {
+      case 'L':
+        result = initNum / galToL;
+        break;
+      case 'kg':
+        result = initNum / lbsToKg;
+        break;
+      case 'km':
+        result = initNum / miToKm;
+        break;
+      case 'gal':
+        result = initNum * galToL;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;
+        
+      default:
+    };
+        
+    return result;
+  };
+  
+  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+    var result = initNum + ' ' + this.spellOutUnit(initUnit) + ' converts to ' + returnNum + ' ' + this.spellOutUnit(returnUnit);
+    
+    return result;
+  };
+  
+}
+
+module.exports = ConvertHandler;
